5d6bdb632bf7814bd39fd1282b09eff129cc74d3
Mithila <mithwick93@gmail.com>
2026-09-07 17:18:34 +0200

    feat: implement Util.truncateExcerpt (homework 3.1)

--- src/main/java/io/spring/Util.java
+++ src/main/java/io/spring/Util.java
@@ -6,6 +6,22 @@ public class Util {
   }
 
   public static String truncateExcerpt(String body, int maxLength) {
-    throw new UnsupportedOperationException();
+    if (body == null || body.isEmpty() || maxLength <= 0) {
+      return "";
+    }
+    if (body.length() <= maxLength) {
+      return body;
+    }
+    int cutIndex = -1;
+    for (int i = maxLength; i >= 0; i--) {
+      if (Character.isWhitespace(body.charAt(i))) {
+        cutIndex = i;
+        break;
+      }
+    }
+    if (cutIndex == -1) {
+      return body.substring(0, maxLength) + "...";
+    }
+    return body.substring(0, cutIndex).replaceAll("\\s+$", "") + "...";
   }
 }
